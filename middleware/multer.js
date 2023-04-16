a491b368cdd4e06bf8a905b1aaec55acc1befa62
deep0133 <deepanshuheer27@gmail.com>
2023-04-16 12:52:33 +0530

    multer limit extended upto 50mb

--- middleware/multer.js
+++ middleware/multer.js
@@ -2,6 +2,11 @@ import multer from "multer";
 
 const storage = multer.memoryStorage();
 
-const singleUpload = multer({ storage }).single("file");
+const singleUpload = multer({
+  storage,
+  limits: {
+    fileSize: 50 * 1024 * 1024, // 50 MB
+  },
+}).single("file");
 
 export default singleUpload;
