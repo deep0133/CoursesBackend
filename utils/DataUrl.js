b72d8e63df3a6f171dc610b7ca304bca4124c185
deep0133 <deepanshuheer27@gmail.com>
2023-04-18 12:05:01 +0530

    file = file.toString()

--- utils/DataUrl.js
+++ utils/DataUrl.js
@@ -2,9 +2,10 @@ import DataUriParser from "datauri/parser.js";
 import path from "path";
 const getDataUri = (file) => {
   // if you already have a file Buffer:
-  console.log("in GetDataUri method File is : " + file);
+  file = file.toString();
   const parser = new DataUriParser();
   const extName = path.extname(file.originalname).toString();
+  console.log("Extension Name: " + extName);
   return parser.format(extName, file.buffer);
 };
 
