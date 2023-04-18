588b38dc2da0ce8e9f834a2dc68130cbe6744772
deep0133 <deepanshuheer27@gmail.com>
2023-04-18 21:13:33 +0530

    removed all unneccessary console.log()

--- utils/DataUrl.js
+++ utils/DataUrl.js
@@ -2,10 +2,8 @@ import DataUriParser from "datauri/parser.js";
 import path from "path";
 const getDataUri = (file) => {
   // if you already have a file Buffer:
-  console.log("In getDataU : file.originalname " + file.originalname);
   const parser = new DataUriParser();
   const extName = path.extname(file.originalname).toString();
-  console.log("Extension Name: " + extName);
   return parser.format(extName, file.buffer);
 };
 
