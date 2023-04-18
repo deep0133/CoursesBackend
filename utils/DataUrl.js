65b57e079ea48e8fef2b09ad2b8097bbae738acd
deep0133 <deepanshuheer27@gmail.com>
2023-04-18 11:58:12 +0530

    show file data

--- utils/DataUrl.js
+++ utils/DataUrl.js
@@ -2,7 +2,7 @@ import DataUriParser from "datauri/parser.js";
 import path from "path";
 const getDataUri = (file) => {
   // if you already have a file Buffer:
-  console.log("in GetDataUri method File is : " + typeof file);
+  console.log("in GetDataUri method File is : " + file);
   const parser = new DataUriParser();
   const extName = path.extname(file.originalname).toString();
   return parser.format(extName, file.buffer);
