3b8d2a20662e6f00f05e13537fe3e49aba7c65aa
deep0133 <deepanshuheer27@gmail.com>
2023-04-18 11:20:52 +0530

    Erro found in daatUri.js - path - undefile

--- utils/DataUrl.js
+++ utils/DataUrl.js
@@ -2,9 +2,10 @@ import DataUriParser from "datauri/parser.js";
 import path from "path";
 const getDataUri = (file) => {
   // if you already have a file Buffer:
-  console.log("in GetDataUri method");
+  console.log("in GetDataUri method File is : " + typeof file);
   const parser = new DataUriParser();
-  const extName = path.extname(file.originalname).toString();
+  const extName = path.extname(file.toString().originalname).toString();
+  console.log("Extension  Name : " + extName);
   return parser.format(extName, file.buffer);
 };
 
