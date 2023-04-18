5d7c4f3f68eb04424942a84285a7c855019c9d43
deep0133 <deepanshuheer27@gmail.com>
2023-04-18 20:48:23 +0530

    checking again

--- utils/DataUrl.js
+++ utils/DataUrl.js
@@ -4,7 +4,7 @@ const getDataUri = (file) => {
   // if you already have a file Buffer:
   console.log("In getDataU : file.originalname " + file.originalname);
   const parser = new DataUriParser();
-  const extName = path.extname(file).toString();
+  const extName = path.extname(file.originalname).toString();
   console.log("Extension Name: " + extName);
   return parser.format(extName, file.buffer);
 };
